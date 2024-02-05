c5d5f12ced462d879f95b2d75b5fea72ce65499f
christiandarnell <97120700+christiandarnell@users.noreply.github.com>
2024-02-06 00:04:03 +0100

    Chnaged __route() to take html attributes

--- src/helpers.php
+++ src/helpers.php
@@ -7,11 +7,16 @@ if (!function_exists('__te')) {
 }
 
 if (!function_exists('__route')) {
-	function __route($key)
+	function __route($key, $attribute = null)
 	{
 		$replace = [];
 		$locale = null;
 		$rte = app('translation.editor')->getTranslation($key, $replace, $locale);
-		return Route::has($rte) ? route($rte) : url($rte . '" target="_blank');
+		$attr = '';
+		if ($attribute) {
+			// Remove last char from string (the ")
+			$attr = '" ' . substr($attribute, 0, -1);
+		}
+		return Route::has($rte) ? route($rte) : url($rte . $attr);
 	}
 }
